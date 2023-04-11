e240806e23494532b16ffcd870a5a73c9e20b47a
Mohammed-5253 <mohammed22091995@gmail.com>
2023-04-11 22:45:20 +0530

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+function registerUser(event) {
+  event.preventDefault(); // Prevents the default form submission behavior
+
+  // Get the form data using the FormData API
+  const formData = new FormData(event.target);
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
 
 
-function registerUser() {
-    const name = document.getElementById('name').value;
+  // Convert the form data into a plain object
+  const formDataObj = {};
+  formData.forEach((value, key) => formDataObj[key] = value);
+
+  // Log the form data in the console
+  console.log(formDataObj);
+  console.log(name);
+  console.log(email);
+  console.log(phone);
+
 }
